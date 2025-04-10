748768fccc6143faad7e44c7967ae22d3c3f3414
columbidaella <ella.katherinecox@gmail.com>
2025-04-10 10:41:06 -0700

    Update Digits.java

--- src/main/java/Digits.java
+++ src/main/java/Digits.java
@@ -6,14 +6,20 @@ public class Digits
 	private ArrayList<Integer> digitList;
 
 	public Digits(int num)
-	{ /* to be implemented in part (a) */ 
-	    
+	{ 
+	 while(num>10){
+		 digitList.add(0,num%10);
+		 num = num/10;
+	 }
+		digitList.add(0,num);
 	}
 
 	public boolean isStrictlyIncreasing()
-	{ /* to be implemented in part (b) */
-		
-
+	{
+	for(int i = 0; i < digitList.size()-1; i++){
+		if(!(digitList.get(i)<digitList.get(i+1))){
+			return false;}}
+		return true;
 	}
 	
 	public String toString()
